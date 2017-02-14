68c339717ff2f01730dde6bca0dfb394b1ed50c1
Steve Wilford <steve@stevewilford.co.uk>
2017-02-14 21:51:59 +0000

    Add test & fake implementation for rendering after a move

--- lib/renderer.rb
+++ lib/renderer.rb
@@ -1,12 +1,21 @@
 class TowersOfHanoiRenderer
   
   def render(game)
-    [
-    '  |     |     |  ',
-    '  -     |     |  ',
-    ' ---    |     |  ',
-    '-----   |     |  ',
-    ]
+    if game.numberOfDiscsOnTower(2) == 1
+      [
+      '  |     |     |  ',
+      '  |     |     |  ',
+      ' ---    |     |  ',
+      '-----   -     |  ',
+      ]
+    else
+      [
+      '  |     |     |  ',
+      '  -     |     |  ',
+      ' ---    |     |  ',
+      '-----   |     |  ',
+      ]
+    end
   end
   
 end
--- spec/renderer_spec.rb
+++ spec/renderer_spec.rb
@@ -14,4 +14,17 @@ describe TowersOfHanoiRenderer, '#render' do
     end
   end
   
+  describe 'move disc 1 to tower 2' do
+    it 'renders 3 towers with disc 2 and 3 on tower 1 and disc 1 on tower 2' do
+      game.move 1, 2
+      expectedOutput = [
+      '  |     |     |  ',
+      '  |     |     |  ',
+      ' ---    |     |  ',
+      '-----   -     |  ',
+      ]
+      expect(renderer.render game).to eq(expectedOutput)
+    end
+  end
+  
 end
